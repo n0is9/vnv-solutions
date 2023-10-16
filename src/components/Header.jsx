1f280fe76604fff3704689235e22ea16dd7ac579
Artem Zakharchuk <zaharchukartem9@gmail.com>
2023-10-16 21:08:39 +0300

    fixed routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,11 +14,11 @@ function Header() {
         <header className='bg-vnv-black border-b md:sticky md:top-0 md:z-50'>
             <Container>
                 <div className='flex flex-col md:flex-row md:justify-between items-center md:pb-0 pb-8'>
-                    <NavLink className='' to='/'><img src={Logo} alt='VNV Solutions' className='w-28'/></NavLink>
+                    <NavLink className='' to='/vnv-solutions'><img src={Logo} alt='VNV Solutions' className='w-28'/></NavLink>
 
                     <ul className='flex justify-between items-center text-sm md:text-md'>
                         <li className='-ml-4 md:-ml-16'>
-                            <NavLink className={navLinkClasses} to='/'>HOME</NavLink>
+                            <NavLink className={navLinkClasses} to='/vnv-solutions'>HOME</NavLink>
                         </li>
                         <li>
                             <NavLink className={navLinkClasses} to='contacts'>CONTACTS</NavLink>
